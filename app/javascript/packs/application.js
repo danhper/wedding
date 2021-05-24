133a7eccace6bb4a3a77eba6740363172311f133
Daniel Perez <daniel@perez.sh>
2021-05-25 01:20:13 +0200

    Fix fontawesome

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,8 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import "@fortawesome/fontawesome-free/css/all";
+import "@fortawesome/fontawesome-free/js/fontawesome.min";
+import "@fortawesome/fontawesome-free/js/regular.min";
 import Rails from "@rails/ujs";
 import Turbolinks from "turbolinks";
 import * as ActiveStorage from "@rails/activestorage";
